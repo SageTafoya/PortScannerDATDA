2e1279db4a1a2bd3bf8106465e254ece5b55d9da
sage-tafoya <tafoyas@oregonstate.edu>
2025-01-20 22:59:27 -0800

    basic socket program

--- portscanner.py
+++ portscanner.py
@@ -1,7 +1,16 @@
 import socket
 
 # Portscanners scan 1 - 65535
-host = socket.gethostname()
+# host = socket.gethostname()
+host = ''
 port = 22 
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# AF_UNSPEC
+
+# for pport in range(65535)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((host, port))
+    s.sendall(b'Hello World')
+    data = s.recv(1024)
+    s.close
+print('Received', repr(data))
